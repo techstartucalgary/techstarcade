2368df7cbb3a1ff2279886ad79774a6d8d2df664
Ray Sandhu <aryamansandhu123@gmail.com>
2023-01-03 12:22:46 -0700

    Artwork with makeshift parallax added. Missing Stalax art and astronaut jumping animation
    Signed-off-by: Ray Sandhu <aryamansandhu123@gmail.com>

--- src/main/games/RocketMan/Assets/Scripts/StalaxBehaviour.cs
+++ src/main/games/RocketMan/Assets/Scripts/StalaxBehaviour.cs
@@ -7,12 +7,16 @@ public class StalaxBehaviour : MonoBehaviour
     //TODO
         // stalax sprites
     public PlayerController playerInfo;
+    private GameObject GroundCeil;
+    private GameObject Background; 
     private Transform _stalax;
     private float _speed;
     private bool _scoreAdded;
     public float playerScore;
     void Start()
     {
+        Background = GameObject.FindGameObjectWithTag("Background");
+        GroundCeil = GameObject.FindGameObjectWithTag("GroundCeil");
         _stalax = gameObject.GetComponent<Transform>();
         playerScore = playerInfo.score;
         _speed = playerScore*0.5f + 5;
@@ -21,11 +25,18 @@ public class StalaxBehaviour : MonoBehaviour
     void Update()
     {
         _stalax.transform.position += Vector3.left * _speed * Time.deltaTime;
+        GroundCeil.transform.position += Vector3.left * _speed * Time.deltaTime *0.25f;
+        Background.transform.position += Vector3.left * _speed * Time.deltaTime * 0.1f;
 
         if (_stalax.transform.position.x <= 0 && !_scoreAdded) {
             _scoreAdded = true;
             playerInfo.scoreChange();
             playerScore = playerInfo.score;
+        }
+
+        if (GroundCeil.transform.position.x <= -100) {
+            Background.transform.position = new Vector3(41, -1);
+            GroundCeil.transform.position = new Vector3(0, 0);            
         }
 
         if (_stalax.transform.position.x <= -10) {
