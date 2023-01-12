f7eb3ced16b3f8f245402530c20351dcc8fbd9f1
Aria Iwazian <101681990+aria21a@users.noreply.github.com>
2023-01-12 16:06:22 -0700

    Tile Logic (#62)
    * Initial Code
    
    * Grid Displayed
    
    * added tile number and mine logic
    
    Signed-off-by: aria21a <101681990+aria21a@users.noreply.github.com>
    
    Signed-off-by: aria21a <101681990+aria21a@users.noreply.github.com>
    Signed-off-by: Madeline Mazurek <81454562+madelinemazurek@users.noreply.github.com>
    Co-authored-by: Madeline Mazurek <81454562+madelinemazurek@users.noreply.github.com>

--- src/main/games/Minesweeper/Assets/Scripts/Game.cs
+++ src/main/games/Minesweeper/Assets/Scripts/Game.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public int width = 8;
-    public int height = 8;
+    static public int width = 16;
+    static public int height = 16;
+    static public int mineCount = width + height;
 
     private Board board;
     private Cell[,] state;
@@ -21,17 +22,88 @@ public class Game : MonoBehaviour
     private void NewGame() {
         state = new Cell[width, height];
         GenerateCells();
+        GenerateMines();
+        GenerateNumbers();
         board.Draw(state);
     }
 
     private void GenerateCells() {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 Cell cell = new Cell();
-                cell.position = new Vector3Int(i, j, 0);
+                cell.position = new Vector3Int(x, y, 0);
                 cell.type = Cell.Type.Empty;
-                state[i, j] = cell;
+                state[x, y] = cell;
             }
         }
+    }
+
+    private void GenerateMines() {
+        for (int i = 0; i < mineCount; i++) {
+            int x = Random.Range(0, width);
+            int y = Random.Range(0, height);
+
+            while (state[x,y].type == Cell.Type.Mine) {
+                x++;
+
+                if (x >= width) {
+                    x = 0;
+                    y++;
+
+                    if (y >= height) {
+                        y = 0;
+                    }
+                }
+            }
+
+            state[x, y].type = Cell.Type.Mine;
+        }
+    }
+
+    private void GenerateNumbers() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Cell cell = state[x, y];
+
+                if (cell.type == Cell.Type.Mine) {
+                    continue;
+                }
+
+                cell.number = CountMines(x, y);
+
+                if (cell.number > 0) {
+                    cell.type = Cell.Type.Number;
+                }
+
+                cell.revealed = true;
+                state[x, y] = cell;
+            }
+        }
+    }
+
+    private int CountMines(int cellX, int cellY) {
+        int count = 0;
+        
+        for (int adjacentX = -1; adjacentX <= 1; adjacentX++) {
+            for (int adjacentY = -1; adjacentY <= 1; adjacentY++) {
+                if (adjacentX == 0 && adjacentY == 0) {
+                    continue;
+                }
+
+                int x = cellX + adjacentX;
+                int y = cellY + adjacentY;
+
+                if (x < 0 || x >= width || y < 0 || y >= height) {
+                    continue;
+                }
+
+                if (state[x, y].type == Cell.Type.Mine) count++;
+            }
+        }
+        return count;
+    }
+
+    private void Update() {
+        
     }
 }
