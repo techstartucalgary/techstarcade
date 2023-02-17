546b265a4e238586c2da5111015836040d2a8cb9
Hana Hassan <hana0hassan0@gmail.com>
2023-02-16 19:23:37 -0700

    New game (#79)
    Signed-off-by: hanahassan0 <hana0hassan0@gmail.com>

---
+++ src/main/games/Journey Home/Assets/Scripts/Ground.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        float speed = GameManager.Instance.gameSpeed / transform.localScale.x;
+        meshRenderer.material.mainTextureOffset += Vector2.right * speed * Time.deltaTime;
+    }
+
+}
