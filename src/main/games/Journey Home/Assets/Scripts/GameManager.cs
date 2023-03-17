f654c99771eb543cd9d03dcfd40533dcde79ce70
Hana Hassan <hana0hassan0@gmail.com>
2023-03-16 19:33:37 -0600

    Hana/journeyhome (#85)
    * New game
    
    Signed-off-by: hanahassan0 <hana0hassan0@gmail.com>
    
    * Game over and replaying
    
    ---------
    
    Signed-off-by: hanahassan0 <hana0hassan0@gmail.com>
    Signed-off-by: Madeline Mazurek <81454562+madelinemazurek@users.noreply.github.com>
    Co-authored-by: Madeline Mazurek <81454562+madelinemazurek@users.noreply.github.com>

--- src/main/games/Journey Home/Assets/Scripts/GameManager.cs
+++ src/main/games/Journey Home/Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
-//using TMPro;
+using TMPro;
 using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine.UI;
 
 // [DefaultExecutionOrder(-1)]
 public class GameManager : MonoBehaviour
@@ -13,11 +13,12 @@ public class GameManager : MonoBehaviour
 
 //     public TextMeshProUGUI scoreText;
 //     public TextMeshProUGUI hiscoreText;
-//     public TextMeshProUGUI gameOverText;
-//     public Button retryButton;
 
-//     private Player player;
-//     private Spawner spawner;
+    public TextMeshProUGUI gameOverText;
+    public Button retryButton;
+
+    private Player player;
+    private Spawner spawner;
 
 //     private float score;
 
@@ -39,44 +40,44 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-//         player = FindObjectOfType<Player>();
-//         spawner = FindObjectOfType<Spawner>();
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
 
         NewGame();
     }
 
     public void NewGame()
     {
-//         Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
 
-//         foreach (var obstacle in obstacles) {
-//             Destroy(obstacle.gameObject);
-//         }
+        foreach (var obstacle in obstacles) {
+            Destroy(obstacle.gameObject);
+        }
 
 //         score = 0f;
         gameSpeed = initialGameSpeed;
-//         enabled = true;
+        enabled = true;
 
-//         player.gameObject.SetActive(true);
-//         spawner.gameObject.SetActive(true);
-//         gameOverText.gameObject.SetActive(false);
-//         retryButton.gameObject.SetActive(false);
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(false);
+        retryButton.gameObject.SetActive(false);
 
 //         UpdateHiscore();
     }
 
-//     public void GameOver()
-//     {
-//         gameSpeed = 0f;
-//         enabled = false;
+    public void GameOver()
+    {
+        gameSpeed = 0f;
+        enabled = false;
 
-//         player.gameObject.SetActive(false);
-//         spawner.gameObject.SetActive(false);
-//         gameOverText.gameObject.SetActive(true);
-//         retryButton.gameObject.SetActive(true);
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(true);
+        retryButton.gameObject.SetActive(true);
 
-//         UpdateHiscore();
-//     }
+        // UpdateHiscore();
+    }
 
     private void Update()
     {
