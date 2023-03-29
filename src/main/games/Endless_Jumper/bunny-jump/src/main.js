cf82b59496a13314fef2b2cc64d86a9868a7550a
Dele <doyelese@gmail.com>
2023-03-29 09:38:36 -0600

    Added intro screen that starts on click or spacebar

--- src/main/games/Endless_Jumper/bunny-jump/src/main.js
+++ src/main/games/Endless_Jumper/bunny-jump/src/main.js
@@ -2,12 +2,13 @@ import Phaser from './lib/phaser.js'
 
 import Game from './scenes/Game.js'
 import GameOver from './scenes/GameOver.js'
+import Title from './scenes/Title.js'
 
 export default new Phaser.Game({
 	type: Phaser.AUTO,
 	width: 480,
 	height: 640,
-	scene: [Game, GameOver],
+	scene: [Title,Game, GameOver],
 	physics: {
 		default: 'arcade',
 		arcade: {
