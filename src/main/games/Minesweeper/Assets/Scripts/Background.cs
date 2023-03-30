ef183eb26b9ff536e6c05059af1f869e4cbc5ec7
aria21a <ariayvazian@gmail.com>
2023-03-30 01:08:13 -0600

    WebGL Optimization
    Signed-off-by: aria21a <ariayvazian@gmail.com>

---
+++ src/main/games/Minesweeper/Assets/Scripts/Background.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject camera = GameObject.Find("Main Camera");
+        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.url = System.IO.Path.Combine (Application.streamingAssetsPath,"Background.mp4");
+        videoPlayer.isLooping = true;
+        videoPlayer.playbackSpeed = 1.5f; 
+    }
+}
