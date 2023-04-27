576da61c74f835ff45587cac9ca010508caf16a3
Madeline Mazurek <81454562+madelinemazurek@users.noreply.github.com>
2023-04-26 23:28:17 -0600

    extension: De-load non-active games and fix refresh issue
    Signed-off-by: Madeline Mazurek <81454562+madelinemazurek@users.noreply.github.com>

--- src/main/popup.js
+++ src/main/popup.js
@@ -27,49 +27,83 @@ document.addEventListener('DOMContentLoaded', function() {
     var f6 = document.getElementById('frameInvader');
     var f7 = document.getElementById('frameTic');
 
+    const s1 = f1.src;
+    const s2 = f2.src;
+    const s3 = f3.src;
+    const s4 = f4.src;
+    const s5 = f5.src;
+    const s6 = f6.src;
+    const s7 = f7.src;
+
+    var cActive = c0;
+    var fActive = f1;
+
     //start on empty screen
     reset();
     c0.style.display = "block";
 
     //buttons display each game
     b0.addEventListener('click', function() {
-        reset();
+        changeFocus();
         c0.style.display = "block";
+        cActive = c0;
+        fActive = f1;
     }, false);
 
     b1.addEventListener('click', function() {
-        reset();
+        changeFocus();
         c1.style.display = "block";
+        cActive = c1;
+        fActive = f1;
+        fActive.src = s1;
     }, false);
 
     b2.addEventListener('click', function() {
-        reset();
+        changeFocus();
         c2.style.display = "block";
+        cActive = c2;
+        fActive = f2;
+        fActive.src = s2;
     }, false);
     
     b3.addEventListener('click', function() {
-        reset();
+        changeFocus();
         c3.style.display = "block";
+        cActive = c3;
+        fActive = f3;
+        fActive.src = s3;
     }, false);
 
     b4.addEventListener('click', function() {
-        reset();
+        changeFocus();
         c4.style.display = "block";
+        cActive = c4;
+        fActive = f4;
+        fActive.src = s4;
     }, false);    
     
     b5.addEventListener('click', function() {
-        reset();
+        changeFocus();
         c5.style.display = "block";
+        cActive = c5;
+        fActive = f5;
+        fActive.src = s5;
     }, false);    
     
     b6.addEventListener('click', function() {
-        reset();
+        changeFocus();
         c6.style.display = "block";
+        cActive = c6;
+        fActive = f6;
+        fActive.src = s6;
     }, false);    
     
     b7.addEventListener('click', function() {
-        reset();
+        changeFocus();
         c7.style.display = "block";
+        cActive = c7;
+        fActive = f7;
+        fActive.src = s7;
     }, false);
     
     //used to reset content
@@ -83,14 +117,19 @@ document.addEventListener('DOMContentLoaded', function() {
         c6.style.display = "none";
         c7.style.display = "none";
 
-        //reloads games so none are running in background
-        f1.src += '';
-        f2.src += '';
-        f3.src += '';
-        f4.src += '';
-        f5.src += '';
-        f6.src += '';
-        f7.src += '';
+        f1.src = '';
+        f2.src = '';
+        f3.src = '';
+        f4.src = '';
+        f5.src = '';
+        f6.src = '';
+        f7.src = '';
+    };
+
+    function changeFocus() {
+        //reloads and hides previous game
+        fActive.src = '';
+        cActive.style.display = "none";
     };
 
 }, false);
